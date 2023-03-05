63eea42f488f04ea03904d333d65f3c6be09033c
BenAbdy <abdyb@macstudent.com.au>
2023-03-06 09:29:38 +1100

    Initial Commit

---
+++ Major Software Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Major Software Project
+//
+//  Created by Benjamin Abdy on 6/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Major Software Project/Major_Software_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Major_Software_ProjectApp.swift
+//  Major Software Project
+//
+//  Created by Benjamin Abdy on 6/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Major_Software_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
